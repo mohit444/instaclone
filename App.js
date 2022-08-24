bc27bf674a4754a435692837cf040b80e0580a38
mohit444 <mksbanking@gmail.com>
2022-08-24 20:25:47 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,15 +1,10 @@
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView } from "react-native";
+import HomeScreen from "./screens/HomeScreen";
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
+    <SafeAreaView>
+      <HomeScreen />
+    </SafeAreaView>
   );
 }
